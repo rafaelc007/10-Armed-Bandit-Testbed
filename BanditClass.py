Fixed a lot of problems and mistakes: using np.average, choosing randomly from ties in argmax function.

BanditClass.py
@@ -10,6 +10,12 @@ class GreedyBandit:
     step_n = 0
     alpha_val = None
 
+    @staticmethod
+    def randargmax(b, **kw):
+        """ a random tie-breaking argmax"""
+        b = np.array(b)
+        return np.argmax(np.random.random(b.shape) * (b == b.max()), **kw)
+
     def alpha(self):
         if self.alpha_val is  None:
             return 1/self.step_n
@@ -27,7 +33,7 @@ class GreedyBandit:
         self.Qn[action] += self.alpha() * (reward - self.Qn[action])
 
     def take_action(self):
-        return np.argmax(self.Qn)
+        return self.randargmax(self.Qn)
 
     def reset(self):
         self.__init__(self.arm_size)
@@ -48,7 +54,7 @@ class EGreedyBandit(GreedyBandit):
 
     def take_action(self):
         if rd.random() > self.eps:
-            return np.argmax(self.Qn)
+            return self.randargmax(self.Qn)
         else:
             return rd.sample(range(self.arm_size), 1)[0]
 

main.py
@@ -27,14 +27,14 @@ def plot_data(best: int, *args, names=[]):
         act_data = data[:, :, 0].astype(int)
         rwd_data = data[:, :, 1]
 
-        ax1.plot(np.sum(rwd_data, axis=0)/rwd_data.shape[0], label=names[n], c=col)
+        ax1.plot(np.average(rwd_data, axis=0), label=names[n], c=col)
         ax1.grid(1)
         ax1.set_xlabel("trial")
         ax1.set_ylabel("reward")
         ax1.legend()
 
         b_data = act_data == best[0]
-        ax2.plot(100*np.sum(b_data, axis=0)/b_data.shape[0], label=names[n], c=col)
+        ax2.plot(100*np.average(b_data, axis=0), label=names[n], c=col)
         ax2.grid(1)
         ax2.set_xlabel("trial")
         ax2.set_ylabel("% of best")
@@ -109,8 +109,8 @@ def run_10_armed(run_type="classic"):
     run_type="deviant" -> Run the deviant 10-armed testbed example requested in exercise 2.5 of the RL book.
     :return: plot graph
     """
-    n_steps = 10000
-    n_mean = 2000
+    n_steps = 800
+    n_mean = 1000
     best, log = run_testbed(n_steps, n_mean, 10, bed_type=run_type, verbose=1)
 
     print("Best choice {}, value: {}".format(best[0], best[1]))
@@ -121,4 +121,4 @@ def run_10_armed(run_type="classic"):
 
 
 if __name__ == "__main__":
-    run_10_armed("deviant")
+    run_10_armed("classic")